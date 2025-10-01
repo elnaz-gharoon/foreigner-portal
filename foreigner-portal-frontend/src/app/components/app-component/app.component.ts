bd32b6cef1b63f7b753c48afe7ac4e29a4c43a28
Elnaz Gharoon <elnazgharoon2020@gmail.com>
2025-10-01 15:48:33 +0200

    NGX Translate installed.

--- foreigner-portal-frontend/src/app/components/app-component/app.component.ts
+++ foreigner-portal-frontend/src/app/components/app-component/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import { TranslateDirective, TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +17,24 @@ import { CommonModule } from '@angular/common';
     MatButtonModule, 
     MatSlideToggleModule, 
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    TranslatePipe,
+    TranslateDirective
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'foreigner-portal-frontend';
+  private translate = inject(TranslateService);
+
+    constructor() {
+        this.translate.addLangs(['de', 'en']);
+        this.translate.setFallbackLang('en');
+        this.translate.use('en');
+    }
+
+    onLanguageSwitch() {
+      console.log('>>>>>>>>>>>>>>>>', this.translate.getCurrentLang());
+      this.translate.use(this.translate.getCurrentLang() === 'en' ? 'de' : 'en');
+    }
 }
