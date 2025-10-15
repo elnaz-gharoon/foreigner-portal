a7a2de8082c809808a31f326655dc921dc637a3b
Ramin Armanfar <ramin.armanfar@gmail.com>
2025-10-15 16:14:23 +0200

    nav bar.

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,14 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbDropdownConfig,
+  NgbDropdownModule,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, NgbDropdownModule],
+  providers: [NgbDropdownConfig],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   @Input() title: string = '';
+
+  constructor(config: NgbDropdownConfig) {
+    config.placement = 'bottom-start';
+
+    // ✅ optional: prevent Popper from auto-flipping
+    config.popperOptions = (options) => ({
+      ...options,
+      modifiers: [
+        ...(options.modifiers || []),
+        {
+          name: 'flip',
+          enabled: false, // disable auto-flipping
+        },
+        {
+          name: 'preventOverflow',
+          options: { boundary: 'viewport' }, // keep inside viewport
+        },
+      ],
+    });
+  }
 }
