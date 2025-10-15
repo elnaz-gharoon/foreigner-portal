19ee306f77222ed24af12916b3e63148c5fe2e85
Ramin Armanfar <ramin.armanfar@gmail.com>
2025-10-15 14:59:21 +0200

    Animation and icon packages added.

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-    selector: 'app-header',
-    standalone: true,
-    imports: [ ],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.scss'
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, NgbDropdownModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  @Input() title: string = '';
 }
