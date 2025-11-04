92114eb6593db122d5f4215ebb2358f71b4fabd2
Elnaz Gharoon <elnazgharoon2020@gmail.com>
2025-11-04 18:37:07 +0100

    Search/page

--- frontend/src/app/pages/search-professions-page/search-professions.component.ts
+++ frontend/src/app/pages/search-professions-page/search-professions.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, Signal } from '@angular/core';
+import {Store} from '@ngxs/store';
+import { Profession } from '../../models/profession.model';
+import { AppState } from '../../state/app.state';
+import { CommonModule } from '@angular/common';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-search-professions',
-  imports: [],
+  imports: [CommonModule, NgbDropdownModule],
   templateUrl: './search-professions.component.html',
   styleUrl: './search-professions.component.scss'
 })
-export class SearchProfessionsComponent {
+export class SearchProfessionsComponent implements OnInit {
 
+  professionsSignal: Signal<Profession[]> = signal([]);
+
+  jobsTitleList: string[] = [];
+
+  constructor(private store: Store) {
+  }
+
+  ngOnInit(): void {
+    this.professionsSignal = this.store.selectSignal(AppState.getProfessions);
+
+    this.jobsTitleList = [...new Set(this.professionsSignal().map(p => p.title))];
+  }
+
+  onJobTitleFilter(jobTitle: string): void {
+    console.log('>>>>>>>>>>>>>>>>JOB TITLE:', jobTitle);
+  }
 }
--- frontend/src/app/state/app.state.ts
+++ frontend/src/app/state/app.state.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
-import { State } from "@ngxs/store";
+import { Selector, State } from "@ngxs/store";
 import { AppStateModel } from "../models/app-state.model";
 import { ConstantValues } from "../utils/constant-values";
+import { Profession } from "../models/profession.model";
 
 @State<AppStateModel>({
     name: 'appState',
@@ -12,4 +13,8 @@ import { ConstantValues } from "../utils/constant-values";
 @Injectable()
 export class AppState {
 
+    @Selector()
+    static getProfessions(state: AppStateModel): Profession[] {
+        return state.professions;
+    }
 }
