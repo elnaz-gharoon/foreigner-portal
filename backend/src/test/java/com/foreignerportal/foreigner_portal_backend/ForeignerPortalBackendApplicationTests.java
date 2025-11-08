d0e665e867e1821cd81d862650ce87f42de71561
Elnaz Gharoon <elnazgharoon2020@gmail.com>
2025-11-08 16:10:54 +0100

    backend project added.

---
+++ backend/src/test/java/com/foreignerportal/foreigner_portal_backend/ForeignerPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foreignerportal.foreigner_portal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignerPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
